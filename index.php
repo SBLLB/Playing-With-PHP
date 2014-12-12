e1baaccb691a29cb30b9985afd92c30b5dba2870
RachelNolan <mail@rachelnolan.com>
2014-12-12 10:39:47 +0000

    Testing echo function

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+  <head> 
+    <title>Playing with PHP</title>
+  </head>
+  <body>
+    <h1>
+      <?php 
+      $welcome = "This is testing the echo function";
+      echo $welcome;
+      ?>
+    </h1>
+  </body>
+</html>
